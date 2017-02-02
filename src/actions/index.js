dd0951b16436e603732b8463ff01dd12dc7076e0
Lavdim Xhelili <lavdimxhelili@gmail.com>
2017-02-02 23:50:10 +0100

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+// API KEY
+const API_KEY = 'e7cea880e3c6b9cdfa7ac0b25f48b841';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+	const url = ROOT_URL + `&q=${city},us`;
+	const request = axios.get(url);
+
+	console.log('Request: ', request)
+
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	}
+}
---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,5 @@
+export default function(state = null, action) {
+	console.log('Action receiver', action);
+	
+	return state;
+}
